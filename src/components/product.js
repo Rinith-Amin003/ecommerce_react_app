9ab295d389be50bd86cb5559f8ddea361d93a0e7
bhavya0711 <bhavyam1107@gmail.com>
2022-06-14 15:18:10 +0530

    new changes

---
+++ src/components/product.js
@@ -0,0 +1,69 @@
+import React from "react";
+
+
+function Product(){
+    return(
+        <div>
+        <div id="hero" class="js-center">
+       <h1>hello</h1>
+        <img src="../img/banner/s1.jpg" class="shop"/>
+        <section id="product11" class="section-p1">
+            <div class="pro-container1">
+                <div class="pro1">
+                
+                    <a href="saree.html"> <img src="/img/women/w1.jpg"/></a>
+                    
+                </div>
+            
+           
+                <div class="pro1" >
+                  <a href="tops.html" ><img src="../assets/img/women/w2.jpg"/></a> 
+                    
+                </div>
+           
+                <div  class="pro11">
+                    <a href="kurti.html" ><img src="../assets/img/women/w3.jpg"/></a>
+                   
+                </div>
+                <div  class="pro11">
+                   <a href="jeans.html" ><img src="../img/women/w4.jpg"/></a>
+                    
+                </div>
+                <div  class="pro11">
+                  <a href="dress.html">  <img src="../img/women/w5.jpg"/></a>
+                    
+                </div> 
+                
+            </div>
+            <img src="../img/banner/s2.jpg" class="shopw"/>
+            <div class="pro-container1">
+                <div class="pro1">
+                   
+                    <a href="tshirt.html"> <img src="../img/men/m1.jpg"/></a>
+                    
+                </div>
+            
+           
+                <div class="pro1">
+                    <a href="shirt.html"> <img src="../img/men/m2.jpg"/></a>
+                  
+                </div>
+                 <div class="pro1">
+                    <img src="../img/men/t.jpg"/>
+                   
+                </div>
+                <div class="pro1">
+                    <img src="../img/men/tr.jpg"/>
+                  
+                </div>
+                <div class="pro1">
+                    <img src="../img/men/j.jpg"/>
+                  
+                </div>
+            </div>
+            </section>
+        </div>
+        </div>
+    )
+}
+export default Product;
